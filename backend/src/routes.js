2e40698ea6bfd48c92072549ba0a54f5c487656d
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-01-15 07:06:10 -0300

    Chamando API do github com Axios

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,22 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const routes = Router();
 
-routes.post("/", (req, res) => {
+routes.post("/devs", async (req, res) => {
   console.log(req.body);
-  return res.json({ message: "Hello Dev!" });
+  // retorno
+  // github_username: 'tiagoalmeida93',
+  // techs: [ 'ReactJS', 'JavaScript', 'C#', 'CSS3' ]
+
+  const { github_username } = req.body;
+
+  const response = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+  console.log(response);
+
+  return res.json({ message: "Hello " + github_username });
 });
 
 module.exports = routes;
