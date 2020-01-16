12c88e0dacfceea9147f61d5a60a03461971c822
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-01-15 22:19:24 -0300

    Inserindo um Dev no mongo

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,22 +1,35 @@
 const { Router } = require("express");
 const axios = require("axios");
+const Dev = require("./models/Dev");
 
 const routes = Router();
 
 routes.post("/devs", async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   // retorno
   // github_username: 'tiagoalmeida93',
-  // techs: [ 'ReactJS', 'JavaScript', 'C#', 'CSS3' ]
+  // techs: 'ReactJS', 'JavaScript', 'C#', 'CSS3'
 
-  const { github_username } = req.body;
+  const { github_username, techs } = req.body;
 
-  const response = await axios.get(
+  const apiResponse = await axios.get(
     `https://api.github.com/users/${github_username}`
   );
-  console.log(response);
 
-  return res.json({ message: "Hello " + github_username });
+  const { name = login, avatar_url, bio } = apiResponse.data;
+
+  const techsArray = techs.split(",").map(tech => tech.trim());
+  // Separando os elementos do array a cada virgula e retirando os espaços pra cada item.
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray
+  });
+
+  return res.json(dev);
 });
 
 module.exports = routes;
