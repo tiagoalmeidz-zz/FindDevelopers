9304c224abb57f08318e3d7cff6ed650e036a5c2
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-01-15 22:55:57 -0300

    Listando Devs

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async list(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs);
+  },
+
+  async insert(req, res) {
+    // console.log(req.body);
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    // procura no Database um Dev com o 'github_username'
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+
+      const { name = login, avatar_url, bio } = apiResponse.data;
+
+      const techsArray = techs.split(",").map(tech => tech.trim());
+      // Separando os elementos do array a cada virgula e retirando os espaços pra cada item.
+
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
+
+    return res.json(dev);
+  }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,35 +1,9 @@
 const { Router } = require("express");
-const axios = require("axios");
-const Dev = require("./models/Dev");
+const DevController = require("./controllers/DevController");
 
 const routes = Router();
 
-routes.post("/devs", async (req, res) => {
-  // console.log(req.body);
-  // retorno
-  // github_username: 'tiagoalmeida93',
-  // techs: 'ReactJS', 'JavaScript', 'C#', 'CSS3'
-
-  const { github_username, techs } = req.body;
-
-  const apiResponse = await axios.get(
-    `https://api.github.com/users/${github_username}`
-  );
-
-  const { name = login, avatar_url, bio } = apiResponse.data;
-
-  const techsArray = techs.split(",").map(tech => tech.trim());
-  // Separando os elementos do array a cada virgula e retirando os espaços pra cada item.
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray
-  });
-
-  return res.json(dev);
-});
+routes.get("/devs", DevController.list);
+routes.post("/devs", DevController.insert);
 
 module.exports = routes;
