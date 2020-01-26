ac2279e948826d02530e64b93fac842fa060df9b
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-01-26 11:37:48 -0300

    Criando rotas

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import Routes from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Tiago!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#1f5921" />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000f43",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-
-  title: {
-    fontWeight: "bold",
-    fontSize: 32,
-    color: "#fff"
-  }
-});
