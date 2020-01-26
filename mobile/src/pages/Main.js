5e35b07475b6806993834f1be57ca7b0026feb43
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-01-26 15:26:49 -0300

    criando input para pesquisar devs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,17 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import {
   getCurrentPositionAsync,
   requestPermissionsAsync
 } from "expo-location";
 import React, { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
 import MapView, { Callout, Marker } from "react-native-maps";
 
 export default function Main({ navigation }) {
@@ -37,32 +45,47 @@ export default function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -22.8276789, longitude: -47.0759815 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://avatars1.githubusercontent.com/u/6900314?s=460&v=4"
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -22.8276789, longitude: -47.0759815 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://avatars1.githubusercontent.com/u/6900314?s=460&v=4"
+            }}
+          />
+
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "tiagoalmeida93"
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Tiago Almeida</Text>
+              <Text style={styles.devBio}>
+                Pós graduado em Engenharia de Software....
+              </Text>
+              <Text style={styles.devTechs}>ReactJS, NodeJS, React Native</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
 
-        <Callout
-          onPress={() => {
-            navigation.navigate("Profile", {
-              github_username: "tiagoalmeida93"
-            });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Tiago Almeida</Text>
-            <Text style={styles.devBio}>
-              Pós graduado em Engenharia de Software....
-            </Text>
-            <Text style={styles.devTechs}>ReactJS, NodeJS, React Native</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -95,5 +118,48 @@ const styles = StyleSheet.create({
 
   devTechs: {
     marginTop: 5
+  },
+
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 5
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#1f5921",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 5
   }
 });
