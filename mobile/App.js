b6517617c3d7144307c5c0efdc0d3fa7ec68e7a5
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-01-26 10:52:15 -0300

    Estilizando o projeto

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Tiago</Text>
+      <Text style={styles.title}>Hello Tiago!</Text>
     </View>
   );
 }
@@ -12,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000f43",
     alignItems: "center",
     justifyContent: "center"
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    color: "#fff"
   }
 });
