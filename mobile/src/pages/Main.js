ff61f2b4df0414fe4768b8c77c3d3859e7f0246f
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-01-26 12:46:53 -0300

    Pegando a localização atual

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,46 @@
-import React from "react";
-import { View } from "react-native";
+import {
+  getCurrentPositionAsync,
+  requestPermissionsAsync
+} from "expo-location";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 export default function Main() {
-  return <View />;
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04, // Zoom do mapa
+          longitudeDelta: 0.04 // Zoom do mapa
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return <MapView initialRegion={currentRegion} style={styles.map} />;
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
